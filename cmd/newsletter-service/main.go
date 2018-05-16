4d40dc1ede5fc8cf15224274de9f5bcd19b36745
Oliver <oliver_Loose@web.de>
2018-05-16 21:39:46 +0200

    adds cli support to start service from cli with flags

--- cmd/newsletter-service/main.go
+++ cmd/newsletter-service/main.go
@@ -6,55 +6,127 @@ import (
 	newsletterServer "newsletter-service/internal/server"
 	"os"
 	"os/signal"
+
+	"time"
+
+	"github.com/urfave/cli"
 )
 
-//TODO: CLI mit urfave flags für dburl, dbname, collection name (constant values)
 //TODO: architecture docu --> alla baustein sicht, gemeinsames dach aus arbeiten (alle), teams dann eigenes; Einfach nicht sonst was großes
 //TODO: docu mit swagger, swaggerUI --> runterladen --> index files -> unter url= das .yaml angeben
-//TODO: HTTPStatusCodes zurückgeben?
 //TODO: Anleitung wie wird microservice gebastelt damit er zum schluss auch läuft
-//TODO: Crosscompile mit go
-//TODO: auf github migrieren mit repo
-//TODO: POST returnen lassen
+//TODO: POST returnen lassen / HTTPStatusCodes zurückgeben?
 
-const (
-	mongodbUrl            = "127.0.0.1:27017"
-	mongodbName           = "newsletterService"
-	mongodbCollectionName = "newsletterCollection"
-	//TODO: constanten mit urfave als Flag machen (EnVar)
-)
+var mongoEnv *MongoEnv
 
-var mongoSession *mongodb.Session
+type MongoEnv struct {
+	session      *mongodb.Session
+	host         string
+	port         string
+	dbName       string
+	dbCollection string
+}
 
 func main() {
-	// establish connection/session to mongodb
-	mongoSession, err := mongodb.NewSession(mongodbUrl)
+	mongoEnv = &MongoEnv{}
+
+	/*
+		Setup CLI
+	*/
+	app := cli.NewApp()
+	app.Name = "newsletterService"
+	app.Version = "1.0.0"
+	app.Compiled = time.Now()
+	app.Authors = []cli.Author{
+		{
+			Name: "Oliver Loose",
+		},
+		{
+			Name: "Ricardo Zirk",
+		},
+	}
+	// define flags/parameters
+	app.Usage = "Start and manage the newsletter micro service"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "db-host, dbh",
+			Value:       "127.0.0.1",
+			Usage:       "Set host of mongodb service",
+			Destination: &mongoEnv.host,
+		},
+		cli.StringFlag{
+			Name:        "db-port, dbp",
+			Value:       ":27017",
+			Usage:       "Set port of mongodb service",
+			Destination: &mongoEnv.port,
+		},
+		cli.StringFlag{
+			Name:        "db-name, dbn",
+			Value:       "newsletterServiceDB",
+			Usage:       "Set name of database to use",
+			Destination: &mongoEnv.dbName,
+		},
+		cli.StringFlag{
+			Name:        "db-collection, dbc",
+			Value:       "newsletterCollection",
+			Usage:       "Set name of collection within database to use",
+			Destination: &mongoEnv.dbCollection,
+		},
+	}
+	// define commands
+	app.Commands = []cli.Command{
+		{
+			Name:   "start",
+			Usage:  "Starts the newsletter micro service",
+			Action: StartNewsletterServer,
+		},
+	}
+
+	// run cli
+	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatalln("Unable to connect to mongodb")
+		log.Fatalf("Error during start up: '%s'", err)
+	}
+}
+
+func StartNewsletterServer(c *cli.Context) error {
+	var err error
+
+	// establish connection/session to mongodb
+	log.Printf("Trying to connect to mongodb on '%s' with database '%s' and collection '%s'",
+		mongoEnv.host+mongoEnv.port, mongoEnv.dbCollection, mongoEnv.dbCollection)
+
+	mongoEnv.session, err = mongodb.NewSession(mongoEnv.host + mongoEnv.port)
+	if err != nil {
+		log.Fatalf("Unable to connect to mongodb: '%s'", err)
+	} else {
+		log.Print("Connected to database")
 	}
 
 	// close session on newsletter-service shutdown
-	defer mongoSession.Close()
+	defer mongoEnv.session.Close()
 
-	// service
-	newsletterService := mongodb.NewNewsletterService(mongoSession.Copy(), mongodbName, mongodbCollectionName)
+	// setup newsletter service for database transactions
+	newsletterService := mongodb.NewNewsletterService(mongoEnv.session.Copy(), mongoEnv.dbName,
+		mongoEnv.dbCollection)
 
-	// server
+	// setup server
 	server := newsletterServer.NewServer(newsletterService)
 	server.Start()
 
-	//gracefully shutdown
+	// gracefully shutdown
 	gracefulStop := make(chan os.Signal, 1)
 	signal.Notify(gracefulStop, os.Interrupt)
 	signal.Notify(gracefulStop, os.Kill)
-
 	go func() {
 		<-gracefulStop
 
-		if mongoSession != nil {
-			mongoSession.Close()
+		if mongoEnv.session != nil {
+			mongoEnv.session.Close()
 		}
 
 		os.Exit(0)
 	}()
+
+	return err
 }
--- internal/db/mongodb/session.go
+++ internal/db/mongodb/session.go
@@ -4,37 +4,36 @@ import (
 	"github.com/globalsign/mgo"
 )
 
-type Session struct{
+type Session struct {
 	session *mgo.Session
 }
 
-func NewSession(mUrl string) (*Session, error){
-	// "localhost:27017 is base path to mongodb
-	session, err := mgo.Dial("localhost:27017")
+func NewSession(mUrl string) (*Session, error) {
+	session, err := mgo.Dial(mUrl)
 
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
 	return &Session{session}, err
 }
 
-func(rSession *Session) Copy() *Session{
+func (rSession *Session) Copy() *Session {
 	return &Session{rSession.session.Copy()}
 }
 
-func(rSession *Session) GetCollection(mDb string, mCollection string) *mgo.Collection{
+func (rSession *Session) GetCollection(mDb string, mCollection string) *mgo.Collection {
 	return rSession.session.DB(mDb).C(mCollection)
 }
 
-func(rSession *Session) Close(){
+func (rSession *Session) Close() {
 	if rSession.session != nil {
 		rSession.session.Close()
 	}
 }
 
-func(rSession *Session) DropDatabase(mDb string) error{
-	if rSession.session != nil{
+func (rSession *Session) DropDatabase(mDb string) error {
+	if rSession.session != nil {
 		return rSession.session.DB(mDb).DropDatabase()
 	}
 
